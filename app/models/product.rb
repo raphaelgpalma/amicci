c032eb8a13388f8c2f76717b7a6e8c69c8c8e131
raphaelgpalma <raphaelgpalma@icloud.com>
2023-12-19 14:14:40 -0300

    db changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
-end
+    has_many_attached :images
+    belongs_to :category, optional: true
+  
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  end
+  
