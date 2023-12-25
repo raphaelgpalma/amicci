e912f47627ac941a9b1eb274142e95f32fb561ad
raphaelgpalma <raphaelgpalma@icloud.com>
2023-12-25 20:38:51 -0300

    adding script.js and some yarn commands that i used by gpt recommendations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// app/javascript/packs/application.js
-import 'HelloWorld'; // Importa o componente HelloWorld.js
 
-//amicci\app\javascript\packs\application.js
