34de4cc55e29f10cf68b81853f62777d85df6017
raphaelgpalma <raphaelgpalma@icloud.com>
2024-01-04 04:24:50 -0300

    back to scratch

--- app/models/product.rb
+++
@@ -1,9 +0,0 @@
-class Product < ApplicationRecord
-    has_many_attached :images
-    belongs_to :category, optional: true
-  
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  end
-  
